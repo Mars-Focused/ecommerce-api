4763524c0069a92ac250ebce6f9ec9360521e2af
Mars_Clarke <marsthecoder@outlook.com>
2020-11-24 14:43:45 -0700

    adding getProducts

--- server/index.js
+++ server/index.js
@@ -1,3 +1,13 @@
+const express = require('express')
+const getProducts = require('./getProducts')
+const products = require('../products.json')
 const app = express()
+const port = 3000
 
-app.listen(3000, () => console.log('Im Hot and Ready'))
+app.get('/api/products', (req, res) => {
+    res.status(200).send(products)
+})
+
+app.listen(port, () => {
+    console.log(`Ready at port: ${port}`)
+})
